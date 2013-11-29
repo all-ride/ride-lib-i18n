8822b609e10723e35f456544e6ceb5e755339380
Joris Vandeweerd <joris@statik.be>
2013-11-29 12:57:58 +0100

    initial commit

---
+++ src/pallo/library/i18n/exception/I18nException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace pallo\library\i18n\exception;
+
+use \Exception;
+
+/**
+ * Exception thrown by the I18n library
+ */
+class I18nException extends Exception {
+
+}
---
+++ src/pallo/library/i18n/locale/negotiator/ChainedNegotiator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace pallo\library\i18n\locale\negotiator;
+
+use pallo\library\i18n\locale\LocaleManager;
+
+/**
+ * Chained locale negotiator
+ */
+class ChainedNegotiator implements Negotiator {
+
+    /**
+     * Negotiators to chain
+     * @var array
+     */
+    protected $negotiators;
+
+    /**
+     * Constructs a new negotiatior chain
+     * @return null
+     */
+    public function __construct() {
+        $this->negotiators = array();
+    }
+
+    /**
+     * Adds a negotiator to the chain, last added negotiator is the first to
+     * be checked, the first added is last.
+     * @param Negotiator $negotiator
+     * @return null
+     */
+    public function addNegotiator(Negotiator $negotiator) {
+        array_unshift($this->negotiators, $negotiator);
+    }
+
+    /**
+     * Determines which locale to use.
+     *
+     * @param pallo\library\i18n\locale\LocaleManager $manager Instance of the
+     * locale manager
+     * @return null|pallo\library\i18n\locale\Locale the locale
+     */
+    public function getLocale(LocaleManager $manager) {
+       foreach ($this->negotiators as $negotiator) {
+           $locale = $negotiator->getLocale($manager);
+           if ($locale) {
+               return $locale;
+           }
+       }
+
+       return null;
+    }
+
+}
